f69de1a577cdb063ddf0da6a00250281b5d500f1
Murat Kairzhanov <murat99010@gmail.com>
2022-12-12 23:38:58 +0600

    building user model

--- base/models.py
+++ base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from .managers import MyUserManager
+
 
 # Create your models here.
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
