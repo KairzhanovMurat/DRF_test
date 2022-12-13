200ddf415c10515cbc914b8747c5686e181afc45
Murat Kairzhanov <murat99010@gmail.com>
2022-12-13 20:34:54 +0600

    added basic http method handlers for apiview

--- base/views.py
+++ base/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import MySerializer
+from rest_framework import status
 
 # Create your views here.
+
+class ProfileApi(APIView):
+    serializer_class = MySerializer
+
+    def get(self, request):
+        data = ['some shit', 'another shit', 'fuck off']
+        return Response({'data': data})
+
+    def post(self, request):
+        name = self.serializer_class(data=request.data)
+        if name.is_valid():
+            _name = name.validated_data.get('name')
+            message = f'Hello {_name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                name.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self,request,pk=None):
+        return Response({'message':'this is put'})
+
+    def patch(self,request,pk=None):
+        return Response({'message':'this is patch'})
+
+    def delete(self,request,pk=None):
+        return Response({'message':'this is delete'})
